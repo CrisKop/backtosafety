71367f3ba5e5e1cbfee63a3e98428436ee3e929c
CrisKop <62496847+CrisKop@users.noreply.github.com>
2020-08-01 10:32:24 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,36 +1,4 @@
-const http = require("http");
-const express = require("express");
-const app = express();
-app.use(express.static("public"));
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/views/index.html");
-});
-app.get("/", (request, response) => {
-  response.sendStatus(200);
-});
 
-setInterval(() => {
-  http.get(`http://terretri.herokuapp.com/`);
-}, 900000);
-
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
-});
-
-
-app.get("/invite", function (request, response) {
-  response.redirect("https://discord.com/oauth2/authorize?client_id=687969316884447260&scope=bot&permissions=8");
-});
-
-app.get("/support", function (request, response) {
-  response.redirect("https://discord.gg/RCNkXaC");
-});
-
-app.get("/", (request, response) => {
-  response.sendStatus(200);
-});
-
-//app.listen(3000);
 
 //start of yout bot xd
 
