061ff6fa34c42943968dbd312744c90940021380
CrisKop <62496847+CrisKop@users.noreply.github.com>
2020-08-01 11:25:48 -0500

    Update botinfo.js

--- cmds/botinfo.js
+++ cmds/botinfo.js
@@ -12,7 +12,7 @@ const allbackups = await backupsdb.find();
 
 message.channel.send( new Discord.RichEmbed()
     .setAuthor(client.user.username+' Info', client.user.avatarURL)
-   .addField('Back To Safety Info:', `**• Creator: **CrisKop#0836\n**• Actual Server: **${message.guild.name}\n**• Prefix:** b!\n**• Version:** 0.1 (Beta)`, true)
+   .addField('Back To Safety Info:', `**• Creator: **CrisKop#0836\n**• Current Server: **${message.guild.name}\n**• Prefix:** b!\n**• Version:** 0.1 (Beta)`, true)
   .addField(`Back To Safety Stats:`, `**• Users: **${client.users.size}\n**• Servers: **${client.guilds.size}\n**• Channels:** ${client.channels.size}\n**• Total Backups:** ${xd}`, true)
                           .setFooter(client.user.username+'#5637', client.user.avatarURL)
                          .setTimestamp()
